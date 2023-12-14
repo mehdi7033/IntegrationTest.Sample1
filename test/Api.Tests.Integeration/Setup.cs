6e488cfa08eceec788842f44e74cb0efce18a3e4
m.ghadimi <m.ghadimi@Alibaba.ir>
2023-12-14 23:23:53 +0330

    chage

--- test/Api.Tests.Integeration/Setup.cs
+++ test/Api.Tests.Integeration/Setup.cs
@@ -2,16 +2,12 @@
 //install-package fluentassertions
 
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Hosting;
 
 
 namespace Api.Tests.Integeration
 {
     public class Setup : WebApplicationFactory<Api.Program>
     {
-        protected override IHost CreateHost(IHostBuilder builder)
-        {
-            return base.CreateHost(builder);
-        }
+
     }
 }
